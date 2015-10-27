6902c12ab08ea27992e8da6062e737a436ee2a73
Wellington Perez <wellingtonaperez@gmail.com>
2015-10-27 12:11:01 +0000

    Added associtation for plans and users

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20151027112624) do
+ActiveRecord::Schema.define(version: 20151027120357) do
 
   create_table "contacts", force: true do |t|
     t.string   "nome"
@@ -41,6 +41,7 @@ ActiveRecord::Schema.define(version: 20151027112624) do
     t.string   "last_sign_in_ip"
     t.datetime "created_at"
     t.datetime "updated_at"
+    t.integer  "plan_id"
   end
 
   add_index "users", ["email"], name: "index_users_on_email", unique: true
